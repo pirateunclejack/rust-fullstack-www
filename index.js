eec4b6f4f024079067102da71255d4e7139ffbef
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-04-23 00:58:15 +0800

    Add wasm-client www

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "wasm-client";
 
-wasm.greet();
+const myForm = document.getElementById("form");
+myForm.addEventListener('submit',(e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const desc = document.querySelector('#description').value;
+
+  wasm.add_course(name, desc).then((json) => {
+    alert('Add successfully');
+    window.location.reload();
+  })
+} )
